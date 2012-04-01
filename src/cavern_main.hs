afbb55d6c4417196cf1b977458ffe772225cea38
Luis Cabellos <zhen.sydow@gmail.com>
2012-04-01 22:44:18 +0200

    change example to use json

--- src/cavern_main.hs
+++ src/cavern_main.hs
@@ -15,15 +15,16 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ----------------------------------------------------------------------------- -}
-module Main( main, testStage, test01 ) where
+module Main( main, test01 ) where
 
 -- -----------------------------------------------------------------------------
 import Control.Monad( forM_ )
 import qualified Graphics.UI.SDL as SDL(
   InitFlag(..), Event(..), init, setVideoMode, waitEvent )
 import System.Posix.Unistd( sleep )
-import Cavern.Render
-import Cavern.Stage
+import Cavern.Render(
+  Camera(..), mkRenderState, runRender, clearScreen, renderStage )
+import Cavern.Scene( Scene(..), loadScene )
 
 -- -----------------------------------------------------------------------------
 mainLoop :: IO ()
@@ -41,25 +42,10 @@ main = do
   mainLoop
 
 -- -----------------------------------------------------------------------------
-testStage :: IO Stage
-testStage = do
-  aa <- loadImage "data/scene01a.png"
-  bb <- loadImage "data/scene01b.png"
-  cc <- loadImage "data/scene01c.png"
-  dd <- loadImage "data/scene01d.png"
-  ss <- loadImage "data/sprite01.png"
-  return $! Stage 1000 480
-    [ Layer 640 480 [ImageProp (return aa)]
-    , Layer 800 480 [ImageProp (return bb)
-                    , ImageProp (return $ moveTo 550 200 ss)]
-    , Layer 800 480 [ImageProp (return $ moveTo 500 130 cc)]
-    , Layer 1000 480 [ImageProp (return $ moveTo 0 309 dd)] ]
-
--- -----------------------------------------------------------------------------
 test01 :: IO ()
 test01 = do
   a <- mkRenderState
-  st <- testStage
+  st <- fmap sceneStage $ loadScene "data/scene01.json"
   forM_ [20,40..360] $ \i -> do
     _ <- runRender (clearScreen >> renderStage st (Camera i 0) ) a
     sleep 1
