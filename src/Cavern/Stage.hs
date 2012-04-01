25433292f4e1bd41db432a7f7a72188fc5ca5fdb
Luis Cabellos <zhen.sydow@gmail.com>
2012-04-01 22:41:31 +0200

    add more getter to ImageProp

--- src/Cavern/Stage.hs
+++ src/Cavern/Stage.hs
@@ -16,27 +16,50 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ----------------------------------------------------------------------------- -}
 module Cavern.Stage(
+  -- * Functions
+  emptyStage, getImageX, getImageY, getImageWidth, getImageHeight,
   -- * Types
   Stage(..), Layer(..), ImageProp(..)
   ) where
 
 -- -----------------------------------------------------------------------------
-import Cavern.Image( Image(..) )
+import Cavern.Image( Image(..), imgWidth, imgHeight )
 
 -- -----------------------------------------------------------------------------
 data ImageProp = ImageProp
              { getImage :: IO Image }
+
+instance Show ImageProp where
+  show _ = "ImageProp"
+
+getImageX :: ImageProp -> IO Int
+getImageX = fmap imgX . getImage
+
+getImageY :: ImageProp -> IO Int
+getImageY = fmap imgX . getImage
+
+getImageWidth :: ImageProp -> IO Int
+getImageWidth = fmap imgWidth . getImage
+
+getImageHeight :: ImageProp -> IO Int
+getImageHeight = fmap imgHeight . getImage
 
 -- -----------------------------------------------------------------------------
 data Layer = Layer
              { layerWidth :: ! Int
              , layerHeight :: ! Int
              , layerImages :: [ImageProp] }
+           deriving( Show )
 
 -- -----------------------------------------------------------------------------
 data Stage = Stage
              { stageWidth :: ! Int
              , stageHeight :: ! Int
              , stageLayers :: [Layer] }
+           deriving( Show )
+
+-- -----------------------------------------------------------------------------
+emptyStage :: Stage
+emptyStage = Stage 0 0 []
 
 -- -----------------------------------------------------------------------------
